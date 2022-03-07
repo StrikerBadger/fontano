20dca46932b52dba50d2b617f8f33c8a69b2b348
Aaron Heiniger <aheiniger@ethz.ch>
2022-03-07 02:55:39 +0100

    Did some work on place cog

--- bot.py
+++ bot.py
@@ -2,11 +2,13 @@ import nextcord
 from nextcord.ext import commands
 import cogs
 from cogs.basic import Basic
+from cogs.place import Place
 import bot_values
 
 bot = commands.Bot(command_prefix=r"fon ")
 bot.remove_command("help")
 
 bot.add_cog(Basic(bot))
+bot.add_cog(Place(bot))
 
 bot.run(bot_values.bot_token)
--- cogs/place.py
+++ cogs/place.py
@@ -3,4 +3,20 @@
 import nextcord
 from nextcord.ext import commands
 from PIL import Image
+import bot_values
 
+class Place(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    # Draw a picture onto ETH-Place
+    @commands.command(description="Starts the drawing process for a given picture (Developer only)")
+    async def draw(self, ctx, filepath, xpos, ypos):
+        if ctx.author.id not in bot_values.aaron_ids:
+            await ctx.send("This command is reserved for the bot's developer.")
+            return
+        image = Image.open(filepath)
+        width = image.width
+        height = image.height
+        color_vals = image.
+        print(str(image.getpixel((0,0))))
