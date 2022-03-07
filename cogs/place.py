d5c6aa4282e2bb43e374bef8fd605c7db90b23d0
Aaron Heiniger <aheiniger@ethz.ch>
2022-03-07 16:18:44 +0100

    Adds first somwhat functioning place draw command

--- cogs/place.py
+++ cogs/place.py
@@ -3,16 +3,13 @@
 import nextcord
 from nextcord.ext import commands
 from PIL import Image
-import time
 import bot_values
 
 
 class Place(commands.Cog):
     def __init__(self, bot):
         self.transparency_bound = 204 # 80% (Alpha value)
-        self.wait_period = 0.5 # 0.5 seconds
         self.draw_channel = 819966095070330950 #eth-place-bots
-        self.burst = 5 # How many pixels to send at once
         self.bot = bot
 
     # Draw a picture onto ETH-Place
@@ -23,24 +20,24 @@ class Place(commands.Cog):
             await ctx.send("This command is reserved for the bot's developer.")
             return
         # Load image, set channel and start placing pixels
-        ctx.channel = self.draw_channel
+        # Set channel here
         image = Image.open(filepath).convert("RGBA")
         width = image.width
         height = image.height
         burst_count = 1
         for i in range(width):
             for j in range(height):
-                x, y = xpos+j, ypos+i
+                x, y = int(xpos)+j, int(ypos)+i
                 pixel = image.getpixel((x, y))
                 # Check for transperancy bound
                 if pixel[3] < self.transparency_bound:
                     continue
-                if burst_count % self.burst == 0:
-                    await time.sleep(self.wait_period)
-                    burst_count = 0
-                await ctx.send(f".place setpixel {x} {y} {self.rgb_to_hex(pixel[:3])}")
-                burst_count += 1
+                await ctx.send(f".place setpixel {x} {y} {self.rgb_to_hex((pixel[:3]))}")
+        image.close()
 
     # To convert rgb values to hex color values
-    def rgb_to_hex(rgb):
-        return f"#{hex(rgb[0])}{hex(rgb[1])}{hex(rgb[2])}"
+    def rgb_to_hex(self, rgb):
+        r = f"0x0{hex(rgb[0])}" if rgb[0] < 16 else str(hex(rgb[0]))
+        g = f"0x0{hex(rgb[1])}" if rgb[1] < 16 else str(hex(rgb[1]))
+        b = f"0x0{hex(rgb[2])}" if rgb[2] < 16 else str(hex(rgb[2]))
+        return f"#{r[-2:]}{g[-2:]}{b[-2:]}"
