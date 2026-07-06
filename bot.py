46e1ae0bbbb2257bdb698ef49f57d57dfd9048d5
Aaron Heiniger <aheiniger@ethz.ch>
2026-07-07 01:48:11 +0200

    Make weather plots look acceptable

--- bot.py
+++ bot.py
@@ -79,7 +79,7 @@ async def briefing_task():
         async with http_client_session.get(WEATHER_URL) as response:
             weather_json = await response.json()
     # Label figures and axis
-    fig, axs = plt.subplots(2, 2)
+    fig, axs = plt.subplots(2, 2, figsize=(1200, 1200, 'px'), gridspec_kw={'wspace':0.25})
     fig.suptitle(f"ETH Zentrum Weather {weather_json['daily']['time']}")
     axs[0, 0].set_title('Temperature (2m)')
     axs[0, 0].set_ylabel(weather_json['hourly_units']['temperature_2m'])
@@ -96,24 +96,28 @@ async def briefing_task():
     # Format the hours once for reuse
     hours = [dt.datetime.fromisoformat(x).hour for x in weather_json['hourly']['time']]
     # Plot the temperature
-    axs[0, 0].bar(hours, weather_json['hourly']['temperature_2m'], label='Air Temperature')
+    axs[0, 0].plot(hours, weather_json['hourly']['temperature_2m'], label='Air Temperature', c='red')
     axs[0, 0].plot(hours, weather_json['hourly']['apparent_temperature'], label='Apparent Temperature')
+    axs[0, 0].grid(which='both')
     axs[0, 0].legend()
     # Plot the precipitation
     cmap = plt.get_cmap('Greys')
-    norm = colors.Normalize(vmin=min(weather_json['hourly']['precipitation']), vmax=max(weather_json['hourly']['precipitation']))
+    norm = colors.Normalize(vmin=0.0, vmax=max(5.0, max(weather_json['hourly']['precipitation'])))
     bar_colors = [cmap(norm(value)) for value in weather_json['hourly']['precipitation']]
     axs[0, 1].bar(hours, weather_json['hourly']['precipitation_probability'], color=bar_colors, label='Precipitation Probability')
+    axs[0, 1].set_ylim(bottom=0.0, top=100.0)
     sm = cm.ScalarMappable(cmap=cmap, norm=norm)
     sm.set_array([])  # Dummy array needed for matplotlib
     cbar = fig.colorbar(sm, ax=axs[0, 1])
-    cbar.set_label(f"Precipitation Amount in {weather_json['hourly']['precipitation']}")
-    axs[0, 1].legend()
+    cbar.set_label(f"Precipitation Amount in {weather_json['hourly_units']['precipitation']}")
     # Plot the UV Index
-    axs[1, 0].bar(hours, weather_json['hourly']['uv_index'])
-    axs[1, 0].set_ylim(12) # Technically could be higher but at a 12 you should not be out in the sun anyway
+    axs[1, 0].bar(hours, weather_json['hourly']['uv_index'], color='orange')
+    if DEBUG:
+        print(weather_json['hourly']['uv_index'])
+    axs[1, 0].set_ylim(bottom=0.0, top=12.0) # Technically could be higher but at a 12 you should not be out in the sun anyway
     # Plot the wind
-    axs[1, 1].bar(hours, weather_json['hourly']['wind_speed_10m'])
+    axs[1, 1].plot(hours, weather_json['hourly']['wind_speed_10m'])
+    axs[1, 1].grid(which='both')
     fig.savefig('weather.png', format='png')
     plt.close(fig)
     await send_briefing()
